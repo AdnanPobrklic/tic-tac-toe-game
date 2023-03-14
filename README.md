5be084dc91be3aaf72dbdeffbf965a268faf005c
Adnan <adnanppobrklic@gmail.com>
2023-03-14 21:46:44 +0100

    full commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Welcome to Tic Tac Toe!
+
+## This is a simple Tic Tac Toe game made with vanilla JavaScript, HTML, and CSS.
+
+To play the game, follow these simple steps:
+
+    1. Open the index.html file in your web browser.
+    2. You will see a 3x3 grid displayed on the screen.
+    3. Player 1 will be X and Player 2 will be O.
+    4. Click on any empty cell in the grid to make a move.
+    5. The first player to get three of their symbols in a row (horizontally, vertically, or diagonally) wins the game.
+    6. If all the cells are filled and no player has three in a row, the game ends in a tie.
+
+### To start a new game, simply refresh the page.
+
+- Have fun playing Tic Tac Toe!
---
+++ main.js
@@ -0,0 +1,80 @@
+const tiles = document.querySelectorAll('.tile')
+let isPlayerXturn = true
+let gameWon = false
+let winner = null
+let values = [    
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' '],
+]
+
+tiles.forEach(item => item.addEventListener('click', clicked))
+
+function clicked(e){
+    if(!gameWon){
+        if(isPlayerXturn){
+            this.innerText = 'X'
+            isPlayerXturn = false
+        }else{
+            isPlayerXturn = true
+            this.innerText = 'O'
+        }
+        addValues(this.classList, this.innerText);
+        checkForWin()
+        this.removeEventListener('click', clicked)
+    }
+
+    if(gameWon){
+        weHaveA_Winner()
+    }
+}
+
+function addValues(index, value){
+    if(index[1].includes('one')){
+        values[0][0] = value
+    }else if(index[1].includes('two')){
+        values[0][1] = value
+    }else if(index[1].includes('three')){
+        values[0][2] = value
+    }else if(index[1].includes('four')){
+        values[1][0] = value
+    }else if(index[1].includes('five')){
+        values[1][1] = value
+    }else if(index[1].includes('six')){
+        values[1][2] = value
+    }else if(index[1].includes('seven')){
+        values[2][0] = value
+    }else if(index[1].includes('eight')){
+        values[2][1] = value
+    }else if(index[1].includes('nine')){
+        values[2][2] = value
+    }
+}
+
+function checkForWin(){
+    for(let i = 0; i <= 2; i++){
+        if(values[i][0] !== ' ' && values[i][0] === values[i][1] && values[i][1] === values[i][2]){
+            gameWon = true;
+            winner = values[i][0]
+        }
+        else if(values[0][i] !== ' ' && values[0][i] === values[1][i] && values[1][i] === values[2][i]){
+            gameWon = true;
+            winner = values[i][0]
+        }
+    }
+
+    if(values[0][0] !== ' ' && values[0][0] === values[1][1] && values[1][1] === values[2][2]){
+        gameWon = true;
+        winner = values[0][0]
+    }else if(values[0][2] !== ' ' && values[0][2] === values[1][1] && values[1][1] === values[2][0]){
+        gameWon = true;
+        winner = values[0][2]
+    }
+}
+
+function weHaveA_Winner(){
+    document.querySelector('h1').style.display = 'none'
+    document.querySelector('.winner-container').style.display = 'flex'
+    document.querySelector('button').addEventListener('click', () => location.reload());
+}
+
